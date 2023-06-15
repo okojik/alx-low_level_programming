c24793a73d0cea34191979948c1d318e679fcaf6
Kingsley <kingsleyokoji@gmail.com>
2023-06-15 15:50:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that uses puts function to print a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
