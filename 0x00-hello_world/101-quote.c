bfd248a217e28afb75b293ddbfc1dd94acc8b575
Kingsley <kingsleyokoji@gmail.com>
2023-06-17 02:37:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a C program that prints a line to a standard error
+ *
+ * return: 1 (Success)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
