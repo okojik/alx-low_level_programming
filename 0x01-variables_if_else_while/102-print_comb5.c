5168a2c6d18bf03a08d1a6054a0b1a825d6627fc
Kingsley <kingsleyokoji@gmail.com>
2023-06-19 05:41:55 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,21 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 98; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j <= 98; j++)
 		{
 			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
 
-				if (i != 98 || j != 98)
+
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i == 98 || j == 99)
 
 				{
 					putchar(',');
@@ -28,6 +30,7 @@ int main(void)
 				}
 			}
 		}
+
 	}
 
 	putchar('\n');
