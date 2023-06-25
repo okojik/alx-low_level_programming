5d0a14dcd5a6d58844b44fc66271aaf940af68b7
Kingsley <kingsleyokoji@gmail.com>
2023-06-25 14:18:11 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,20 +21,15 @@ int main()
 		{
 			printf(" Fizz");
 		}
-		else
-
-			if (i % 5 == 0 && 3 != 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 			{
 				printf(" Buzz");
 			}
-			else
-
-				if (i % 3 == 0 && 5 == 0)
+			else if (i % 3 == 0 && i % 5 == 0)
 				{
 					printf(" FizzBuzz");
 				}
-				else
-					if (i == 1)
+				else if (i == 1)
 					{
 					printf("%d", i);
 					}
