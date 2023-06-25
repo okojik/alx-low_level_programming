917e6f6d0f6a45893f9332c248cac41ca1fa9eee
Kingsley <kingsleyokoji@gmail.com>
2023-06-25 14:46:00 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,18 +16,20 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = i; j < size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
-		}
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
 
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
+
