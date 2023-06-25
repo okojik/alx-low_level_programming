133df1a341527e777fcccf2b7e3d6f2f5cd4bdd6
Kingsley <kingsleyokoji@gmail.com>
2023-06-25 15:40:26 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,10 +6,10 @@
  */
 void print_number(int n)
 {
-	if (n < 0) 
+	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+	_putchar('-');
+	n = -n;
 	}
 
 	if (n / 10)
@@ -17,4 +17,3 @@ void print_number(int n)
 
 	_putchar((n % 10) + '0');
 }
-
