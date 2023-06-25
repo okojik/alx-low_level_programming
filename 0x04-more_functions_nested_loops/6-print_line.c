5f231d529982105d96570e468f9d9aab54d1d77b
Kingsley <kingsleyokoji@gmail.com>
2023-06-25 13:30:09 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,7 +10,7 @@
 void print_line(int n)
 {
 
-	if ( n <= '0')
+	if ( n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -18,7 +18,7 @@ void print_line(int n)
 	{
 		int i;
 
-		for (i = '1'; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
