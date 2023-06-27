9fe989ef7d8dd2f56925b5c5757a54de611887d4
Kingsley <kingsleyokoji@gmail.com>
2023-06-26 17:16:23 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,24 @@
  */
 void puts2(char *str)
 {
+	int length = 0;
 	int i = 0;
-	while (str[i] != '\0')
+	char *y = str;
+	int j;
+
+	while (*y != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		y++;
+		length++;
+	}
+	i = length - 1;
+	for (j = 0 ; j <= i ; j++)
+	{
+		if (j % 2 == 0)
+	{
+		_putchar(str[j]);
+	}
 	}
 	_putchar('\n');
 }
+
