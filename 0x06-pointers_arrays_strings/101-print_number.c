57121184eea3aea39227e4f8e9efa0db446257fc
Kingsley <kingsleyokoji@gmail.com>
2023-07-01 16:00:25 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,14 +10,14 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -n;
+		n = -n;
 	}
 
-	if (num / 10 != 0)
+	if (n / 10 != 0)
 	{
-		print_number(num / 10);
+		print_number(n / 10);
 	}
 
-	_putchar((num % 10) + '0');
+	_putchar((n % 10) + '0');
 }
 
